418e52f5b07f36425847cee557425f784545fc08
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2023-01-20 16:31:36 +0700

    move to zero

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/:col/:key', async (req, res) => {
   res.json(item).end()
 })
 
-// Get a full listing
+//Get a full listing
 // app.get('/:col', async (req, res) => {
 //   const col = req.params.col
 //   console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
@@ -60,67 +60,15 @@ app.get('/:col/:key', async (req, res) => {
 //   console.log(JSON.stringify(items, null, 2))
 //   res.json(items).end()
 // })
-  
-//get all
-  app.get('/:col', async (req, res) => {
-    const col = req.params.col
-    //console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-    const items = await db.collection(col).list()
-    //const item = await db.collection(col).get(key)
-    //console.log(JSON.stringify(items, null, 2))
-    //console.log(col) //nama colom (cekgung)
-    //console.log(items) 
-    // {
-    //   results: [
-    //     { collection: 'cekgung', key: 'id', props: [Object] },
-    //     { collection: 'cekgung', key: 'asu', props: [Object] },
-    //     { collection: 'cekgung', key: 'asu2', props: [Object] }
-    //   ]
-    // }
-    // arrayA = items.results
-    // arrayA.map(function(value) {
-    //   return value.key
-    // });
-    // console.log(arrayA)
-    // res.json(items).end()
-    //let akhir = res.json(items).end()
-    //let array = []
-    //for (){
-    //  array.push(value)
-    //}
-    //console.log(array)
 
-  //   export const scanTable = async (tableName) => {
-  //     const params = {
-  //         TableName: tableName,
-  //     };
-  
-  //     const scanResults = [];
-  //     const items;
-  //     do{
-  //         items =  await documentClient.scan(params).promise();
-  //         items.Items.forEach((item) => scanResults.push(item));
-  //         params.ExclusiveStartKey  = items.LastEvaluatedKey;
-  //     }while(typeof items.LastEvaluatedKey !== "undefined");
-      
-  //     return scanResults;
-  
-  // };
-  // })
+app.get('/:col', async (req, res) => {
+  const col = req.params.col
+  console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
+  const items = await db.collection(col).list()
+  console.log(JSON.stringify(items, null, 2))
+  res.json(items).end()
 
-  var dynamoClient = new AWS.DynamoDB.DocumentClient();
-  var params = {
-    TableName: col, // give it your table name 
-    Select: "ALL_ATTRIBUTES"
-  };
-
-  dynamoClient.scan(params, function(err, data) {
-    if (err) {
-       console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-       console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-    }
-  })
+  
 })
 
 // Catch all handler for all other request.
