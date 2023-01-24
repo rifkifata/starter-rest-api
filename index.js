981b34ff7baa8ab2d2c883de96158bf8b2272292
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2023-01-24 10:05:45 +0700

    aborts

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.get('/:col', async (req, res) => {
 
 })
 
-// Catch all handler for all other request.
+// Catch all handler for all other request..
 app.use('*', (req, res) => {
   res.json({ msg: 'no route handler found' }).end()
 })
