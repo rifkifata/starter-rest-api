21c75ebcf0d5f2ec07b5cfc7e7fd0efd3040e1f6
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2023-01-24 10:10:29 +0700

    aborts

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
-let AWS = require('aws-sdk');
+
+const { ObjectID } = require('mongodb');
+//const ObjectID = require('mongodb').ObjectID
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -21,13 +23,22 @@ app.use(express.urlencoded({ extended: true }))
 // #############################################################################
 
 // Create or Update an item
-app.post('/:col/:key', async (req, res) => {
+app.post('/:col', async (req, res) => {
   console.log(req.body)
+    const now = new Date();
+    let date = {
+        "createdAt": now.toISOString(),
+        "updatedAt": now.toISOString()
+    }
+    const isi = {...req.body, ...date}
 
+  const objectId = new ObjectID();
   const col = req.params.col
-  const key = req.params.key
+  //const key = req.params.key
+  let key = objectId.toString()
+
   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
+  const item = await db.collection(col).set(key, isi)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
 })
@@ -43,55 +54,80 @@ app.delete('/:col/:key', async (req, res) => {
 })
 
 // Get a single item
-app.get('/:col/:key', async (req, res) => {
+app.get('/getbykey/:col/:key', async (req, res) => {
   const col = req.params.col
   const key = req.params.key
   console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).get(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
+  let item = await db.collection(col).get(key)
+    let props = item.props
+    delete props.updated
+    delete props.created
+    let newitem = {
+        key: key,
+        collection : col,
+        ...props
+    }
+  console.log(JSON.stringify(newitem, null, 2))
+  res.json(newitem).end()
 })
 
-//Get a full listing
-// app.get('/:col', async (req, res) => {
-//   const col = req.params.col
-//   console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-//   const items = await db.collection(col).list()
-//   console.log(JSON.stringify(items, null, 2))
-//   res.json(items).end()
-// })
+//Get all full listing
+ /*app.get('/getfull/:col', async (req, res) => {
+   const col = req.params.col
+   console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
+   const items = await db.collection(col).list()
+   console.log(JSON.stringify(items, null, 2))
+   res.json(items).end()
+ })*/
 
-app.get('/:col', async (req, res) => {
+//get All
+app.get('/getall/:col', async (req, res) => {
   const col = req.params.col
   console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
   const items = await db.collection(col).list()
-  console.log(JSON.stringify(items, null, 2))
-  res.json(items).end()
-  // let arrayOfObject = items.results.map((item) => ({
-  //   key: item.key
-  // }))
-  //console.log(abc)
   let result = items.results.map(a => a.key)
-  console.log(result)
+  let currentArray = []
+  
+    await Promise.all(
+        result.map(async (item) => {
+            currentArray.push(await db.collection(col).get(item))
+        })
+    )
 
-  //nembak by key
-  let penampung = []
-  for (let i = 0;i<3;i++){
-    const key = result[i]
-    const item = await db.collection(col).get(key)
-    penampung.push(item)
-    console.log(JSON.stringify(items, null, 2))
-  }
-  console.log("hasil akhir : " + penampung)
+    currentArray.map(item => {
+        Object.assign(item, item.props)
+        delete item.props;
+        return item
+    })
 
+    let finalResult = { "results": currentArray }
+    res.json(finalResult).end()
 })
 
-// Catch all handler for all other request..
+// Update entire bike
+app.put("/:col/:key", async (req, res) => {
+    const key = req.params.key
+    const col = req.params.col
+
+    // Delete existing object
+    await db.collection(col).delete(key)
+
+    if (!req.body.updatedAt) {
+        
+    }
+    
+    // Save new Object
+    const item = await db.collection(col).set(key, req.body)
+    console.log(JSON.stringify(item, null, 2))
+    res.json(item).end()
+});
+
+// Catch all handler for all other request.
 app.use('*', (req, res) => {
   res.json({ msg: 'no route handler found' }).end()
 })
 
-// Start the server
+// Start the servers
 const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`index.js listening on ${port}`)
