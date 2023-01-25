e1bc2b67b775d6a14b5b5df9832458cb69f52c57
teuku rifki dhulul fata <rifkifata@gmail.com>
2023-01-25 12:20:32 +0700

    solve corse issue

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const { ObjectID } = require('mongodb');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:9000/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
