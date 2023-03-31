7d29df1d89c1881a2a642792924b392bbdb4a1b9
teuku rifki dhulul fata <rifkifata@gmail.com>
2023-03-31 21:23:41 +0700

    coba async

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ async function sendMessage() {
        method: "POST",
        json: {
          "chatId": "6285277494909@c.us",
-         "message": msg}
+         "message": "asdsd"}
        }, function(error, response, data) {
          console.error('error:', error); // Print the error if one occurred
          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
