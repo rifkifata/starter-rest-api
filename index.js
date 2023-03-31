2811caef49163f4d17c0799c0bc6d75d17887f4b
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2023-03-31 10:46:32 +0700

    change api to send message + add tanggal

--- index.js
+++ index.js
@@ -206,9 +206,11 @@ app.get('/anyapi', async(req, res, next) => {
         method: "POST",
         json: {
           "chatId": "6285277494909@c.us",
-          "message": msg
-      }}, function(error, response, data) {
-           
+          "message": msg}
+        }, function(error, response, data) {
+          console.error('error:', error); // Print the error if one occurred
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          console.log('body:', body); // Print the HTML for the Google homepage.
       });
     })
 });
