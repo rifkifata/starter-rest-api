bf48418d09014ecb831b6128cf779f3f5d511aae
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2023-10-12 23:06:43 +0700

    adjust judul

--- index.js
+++ index.js
@@ -405,7 +405,7 @@ function doRequest(options) {
       console.log(arr)
 
       // ADD JUDUL
-      const judul = options.url.toString().slice(30, -1).replace("/","")
+      let judul = options.url.toString().slice(30, -1).replace("/","").replace("-"," ")
       console.log(judul);
       
       resolve(arr)
