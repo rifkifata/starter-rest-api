47cd84340a094e31632e7cd8034324a80057ad57
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2023-10-13 14:24:24 +0700

    checking for 404

--- index.js
+++ index.js
@@ -425,7 +425,9 @@ function doRequest(options) {
             // if (error) {
             //   arr.splice(i, 1)
             // }
-            console.log(response.statusCode)
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.
           });
         }
 
